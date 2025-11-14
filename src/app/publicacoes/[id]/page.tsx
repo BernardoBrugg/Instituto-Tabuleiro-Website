f136d26acf374669f1354d986e9e0c79858c13fc
BernardoBrugg <bbbrugg@gmail.com>
2025-11-14 13:59:12 -0300

    publications updates with photos

--- src/app/publicacoes/[id]/page.tsx
+++ src/app/publicacoes/[id]/page.tsx
@@ -7,45 +7,130 @@ import Link from "next/link";
 import { publications } from "./publications";
 
 const folderNames: Record<number, string> = {
-  1: '3.1. Publicação do Livro “Criador de Peixe, Criador de Gente_ a pesca, a vida e a natureza do Estuário do Rio da Madre”',
-  2: '3.2. Programa de Pesquisa “Felinos do Tabuleiro”',
-  3: '3.3.  Plano de Ação Estadual (PAE) para a conservação do Preá-de-Moleques-do-Sul',
-  4: '3.4. Projeto de Pesquisa “Monitoramento da flora da restinga da Baixada do Massiambu inserida no Parque Estadual da Serra do Tabuleiro_ avaliação histórica e correlação das condições natural e pós-distúrbios antrópicos” ',
-  5: '3.5. Projeto de Pesquisa “Avaliação de procedimentos de monitoramento populacional não invasivo do Preá-de-Moleques (Cavia intermedia) endêmico da ilha de Moleques do Sul, Parque Estadual da Serra do Tabuleiro”',
-  6: '3.6. Projeto de Pesquisa “Conservação ex-situ de Commelina catharinensis Hassemer et al. (Commelinaceae)”',
-  7: '3.7.  “Programa de Educação Ambiental para Conservação da espécie endêmica Cavia intermedia, o Preá do Arquipélago de Moleques do Sul - Santa Catarina - Brasil” ',
-  8: '3.8. Projeto_ Refloresta Maciambu_ Recuperação de mata ciliar e troca de saberes na Terra Indígena Maciambu (Aldeia Pira Rupá-SC) ',
-  9: 'Fotos extras',
+  1: "3.1. Publicação do Livro “Criador de Peixe, Criador de Gente_ a pesca, a vida e a natureza do Estuário do Rio da Madre”",
+  2: "3.2. Programa de Pesquisa “Felinos do Tabuleiro”",
+  3: "3.3.  Plano de Ação Estadual (PAE) para a conservação do Preá-de-Moleques-do-Sul",
+  4: "3.4. Projeto de Pesquisa “Monitoramento da flora da restinga da Baixada do Massiambu inserida no Parque Estadual da Serra do Tabuleiro_ avaliação histórica e correlação das condições natural e pós-distúrbios antrópicos” ",
+  5: "3.5. Projeto de Pesquisa “Avaliação de procedimentos de monitoramento populacional não invasivo do Preá-de-Moleques (Cavia intermedia) endêmico da ilha de Moleques do Sul, Parque Estadual da Serra do Tabuleiro”",
+  6: "3.6. Projeto de Pesquisa “Conservação ex-situ de Commelina catharinensis Hassemer et al. (Commelinaceae)”",
+  7: "3.7.  “Programa de Educação Ambiental para Conservação da espécie endêmica Cavia intermedia, o Preá do Arquipélago de Moleques do Sul - Santa Catarina - Brasil” ",
+  8: "3.8. Projeto_ Refloresta Maciambu_ Recuperação de mata ciliar e troca de saberes na Terra Indígena Maciambu (Aldeia Pira Rupá-SC) ",
+  9: "Fotos extras",
 };
 
 const imageLists: Record<number, string[]> = {
-  1: ['Canon EOS 6D_20180320_112232.Id_2750.jpg', 'IMG_3926_pb.jpg', 'IMG_6436_pb.jpg', 'IMG_7524_pb.jpg'],
-  2: ['WhatsApp Image 2025-08-08 at 13.43.20 (1).jpeg', 'WhatsApp Image 2025-08-08 at 13.43.20.jpeg', 'WhatsApp Image 2025-08-08 at 13.43.21 (1).jpeg', 'WhatsApp Image 2025-08-08 at 13.43.21 (2).jpeg', 'WhatsApp Image 2025-08-08 at 13.43.21.jpeg', 'WhatsApp Image 2025-08-08 at 13.43.22.jpeg', 'WhatsApp Image 2025-08-08 at 13.48.16 (1).jpeg', 'WhatsApp Image 2025-08-08 at 13.48.16.jpeg', 'WhatsApp Image 2025-08-08 at 13.48.17 (1).jpeg', 'WhatsApp Image 2025-08-08 at 13.48.17.jpeg', 'd7e253_e7860a31fee9441bbe64d14cd1e00611.jpg'],
-  3: ['DSCF3874.JPG', 'DSCF3905.JPG', 'Logo Pró-preá_versões_Prancheta 1.jpg', 'WhatsApp Image 2019-04-12 at 03.08.36 (2).jpeg'],
-  4: ['Acisanthera aff variabilis cordaoCV Manoel 14 jan (2).JPG', 'Acisanthera cf variabilis resgate dez 21 (2).JPG', 'Baccharis aff linearifolia CV MariPascoa 20 abr (7).jpg', 'CV guris 2 set21 (10).JPG', 'Cad_Jhon ago21 (1).JPG', 'Calea uniflora Cang 18 set (6).JPG', 'Calea uniflora Intro Com nov23 (14).JPG', 'Chromolaena ulei CV MariPascoa 20 abr (4).JPG', 'Coccocypselum cf capitatum Com3M 8 set (6).JPG', 'Cyrtocymura scorpioides CV Jhon 14 out (6).JPG', 'Desmodium barbatum cordaoCVManoel 14jan (2).JPG', 'Eugenia handroana pluriflora Cang 18 set (2).JPG', 'Eugenia handroana pluriflora Cang 18 set (6).JPG', 'IMG_20220609_122744_099.jpg', 'Mandevilla coccinea cordaoCV bomb23 jan (3).JPG', 'Miconia ligustroides monit nov23 (49).JPG', 'Monit Raizes 15dez23 (1).JPG', 'RGA01876.jpg', 'T1Arb 31 out22 (2).JPG', 'T_Arb5 P3 Gato 16 jun (24).JPG', 'T_Arb6 Espanhol 19 ago22 (9).jpeg'],
-  5: ['2021-12-28 11.13.44.jpg', '2021-12-28 11.17.37.jpg', '2021-12-28 11.58.40.jpg', '2021-12-28 13.43.25.jpg', '2021-12-28 13.43.38.jpg', '2021-12-28 20.57.58.jpg', '2021-12-28 20.58.04-1.jpg', '2021-12-31 16.26.30-1.jpg'],
-  6: ['C21 abr 23 (47).JPG', 'DSCN0086.JPG', 'Introdução 8_3_23 Felipe (4).JPG', 'Marcelo Intro mar 23 (11).jpeg', 'Monit Introdução fev 23 (4).JPG', 'N5 abr 23 (4).JPG'],
-  7: ['IMG_2154-3.jpg', 'IMG_3748 (2).jpg', 'IMG_4281.jpg', 'IMG_5310.JPEG', 'IMG_5397 (2).JPG', 'PHOTO-2024-03-21-13-33-45.jpg'],
-  8: ['Refloresta Maciambu 1.jpg', 'Refloresta Maciambu 2.jpg', 'Refloresta Maciambu 3.jpg', 'Refloresta Maciambu 4.jpg'],
-  9: ['Canon EOS 6D_20180313_174530.jpg', 'DSC01886.JPG', 'DSC_7455.JPG', 'DSC_7461.JPG', 'DSC_7486.JPG', 'DSC_7501.JPG', 'DSC_9044.JPG', 'IMG_0078.JPG', 'capa.jpg'],
+  1: [
+    "Canon EOS 6D_20180320_112232.Id_2750.jpg",
+    "IMG_3926_pb.jpg",
+    "IMG_6436_pb.jpg",
+    "IMG_7524_pb.jpg",
+  ],
+  2: [
+    "WhatsApp Image 2025-08-08 at 13.43.20 (1).jpeg",
+    "WhatsApp Image 2025-08-08 at 13.43.20.jpeg",
+    "WhatsApp Image 2025-08-08 at 13.43.21 (1).jpeg",
+    "WhatsApp Image 2025-08-08 at 13.43.21 (2).jpeg",
+    "WhatsApp Image 2025-08-08 at 13.43.21.jpeg",
+    "WhatsApp Image 2025-08-08 at 13.43.22.jpeg",
+    "WhatsApp Image 2025-08-08 at 13.48.16 (1).jpeg",
+    "WhatsApp Image 2025-08-08 at 13.48.16.jpeg",
+    "WhatsApp Image 2025-08-08 at 13.48.17 (1).jpeg",
+    "WhatsApp Image 2025-08-08 at 13.48.17.jpeg",
+    "d7e253_e7860a31fee9441bbe64d14cd1e00611.jpg",
+  ],
+  3: [
+    "DSCF3874.JPG",
+    "DSCF3905.JPG",
+    "Logo Pró-preá_versões_Prancheta 1.jpg",
+    "WhatsApp Image 2019-04-12 at 03.08.36 (2).jpeg",
+  ],
+  4: [
+    "Acisanthera aff variabilis cordaoCV Manoel 14 jan (2).JPG",
+    "Acisanthera cf variabilis resgate dez 21 (2).JPG",
+    "Baccharis aff linearifolia CV MariPascoa 20 abr (7).jpg",
+    "CV guris 2 set21 (10).JPG",
+    "Cad_Jhon ago21 (1).JPG",
+    "Calea uniflora Cang 18 set (6).JPG",
+    "Calea uniflora Intro Com nov23 (14).JPG",
+    "Chromolaena ulei CV MariPascoa 20 abr (4).JPG",
+    "Coccocypselum cf capitatum Com3M 8 set (6).JPG",
+    "Cyrtocymura scorpioides CV Jhon 14 out (6).JPG",
+    "Desmodium barbatum cordaoCVManoel 14jan (2).JPG",
+    "Eugenia handroana pluriflora Cang 18 set (2).JPG",
+    "Eugenia handroana pluriflora Cang 18 set (6).JPG",
+    "IMG_20220609_122744_099.jpg",
+    "Mandevilla coccinea cordaoCV bomb23 jan (3).JPG",
+    "Miconia ligustroides monit nov23 (49).JPG",
+    "Monit Raizes 15dez23 (1).JPG",
+    "RGA01876.jpg",
+    "T1Arb 31 out22 (2).JPG",
+    "T_Arb5 P3 Gato 16 jun (24).JPG",
+    "T_Arb6 Espanhol 19 ago22 (9).jpeg",
+  ],
+  5: [
+    "2021-12-28 11.13.44.jpg",
+    "2021-12-28 11.17.37.jpg",
+    "2021-12-28 11.58.40.jpg",
+    "2021-12-28 13.43.25.jpg",
+    "2021-12-28 13.43.38.jpg",
+    "2021-12-28 20.57.58.jpg",
+    "2021-12-28 20.58.04-1.jpg",
+    "2021-12-31 16.26.30-1.jpg",
+  ],
+  6: [
+    "C21 abr 23 (47).JPG",
+    "DSCN0086.JPG",
+    "Introdução 8_3_23 Felipe (4).JPG",
+    "Marcelo Intro mar 23 (11).jpeg",
+    "Monit Introdução fev 23 (4).JPG",
+    "N5 abr 23 (4).JPG",
+  ],
+  7: [
+    "IMG_2154-3.jpg",
+    "IMG_3748 (2).jpg",
+    "IMG_4281.jpg",
+    "IMG_5310.JPEG",
+    "IMG_5397 (2).JPG",
+    "PHOTO-2024-03-21-13-33-45.jpg",
+  ],
+  8: [
+    "Refloresta Maciambu 1.jpg",
+    "Refloresta Maciambu 2.jpg",
+    "Refloresta Maciambu 3.jpg",
+    "Refloresta Maciambu 4.jpg",
+  ],
+  9: [
+    "Canon EOS 6D_20180313_174530.jpg",
+    "DSC01886.JPG",
+    "DSC_7455.JPG",
+    "DSC_7461.JPG",
+    "DSC_7486.JPG",
+    "DSC_7501.JPG",
+    "DSC_9044.JPG",
+    "IMG_0078.JPG",
+    "capa.jpg",
+  ],
 };
 
 const imagesMap: Record<number, string[]> = {};
 for (const idStr in imageLists) {
   const id = parseInt(idStr, 10);
-  imagesMap[id] = imageLists[id].map((img: string) => `/${encodeURIComponent(folderNames[id])}/${encodeURIComponent(img)}`);
+  imagesMap[id] = imageLists[id].map(
+    (img: string) =>
+      `/${encodeURIComponent(folderNames[id])}/${encodeURIComponent(img)}`
+  );
 }
 
 const layoutMap: Record<number, string> = {
-  1: 'grid-cols-2',
-  2: 'grid-cols-3',
-  3: 'grid-cols-2',
-  4: 'grid-cols-4',
-  5: 'grid-cols-4',
-  6: 'grid-cols-3',
-  7: 'grid-cols-3',
-  8: 'grid-cols-2',
-  9: 'grid-cols-4',
+  1: "grid-cols-2",
+  2: "grid-cols-3",
+  3: "grid-cols-2",
+  4: "grid-cols-4",
+  5: "grid-cols-4",
+  6: "grid-cols-3",
+  7: "grid-cols-3",
+  8: "grid-cols-2",
+  9: "grid-cols-4",
 };
 
 export default function PublicationDetailPage() {
@@ -93,7 +178,7 @@ export default function PublicationDetailPage() {
         >
           <Header />
           <div className="flex-grow flex flex-col items-center justify-center text-center">
-            <h2 className="text-5xl md:text-5xl font-bold tracking-tight text-white leading-tight hover:text-white hover:scale-105 transition-all duration-700 cursor-pointer">
+            <h2 className="text-5xl md:text-5xl font-bold tracking-tight text-white leading-tight ">
               {publication.title}
             </h2>
             <p className="mt-4 max-w-xl text-white/80 hover:text-white/90 hover:scale-105 transition-colors duration-700 ease-in-out cursor-pointer">
@@ -121,10 +206,7 @@ export default function PublicationDetailPage() {
               />
               <div>
                 {publication.description.split(". ").map((sentence, index) => (
-                  <p
-                    key={index}
-                    className="text-white/80 mb-4"
-                  >
+                  <p key={index} className="text-white/80 mb-4">
                     {sentence}.
                   </p>
                 ))}
@@ -147,10 +229,7 @@ export default function PublicationDetailPage() {
               </div>
               <div>
                 {publication.description.split(". ").map((sentence, index) => (
-                  <p
-                    key={index}
-                    className="text-white/80 mb-4 cursor-pointer"
-                  >
+                  <p key={index} className="text-white/80 mb-4 cursor-pointer">
                     {sentence}.
                   </p>
                 ))}
@@ -173,16 +252,20 @@ export default function PublicationDetailPage() {
             hover:bg-black/30 hover:shadow-2xl hover:scale-[1.02] transition-all duration-700"
           >
             <div className={`grid ${layoutMap[id]} gap-4`}>
-              {imagesMap[id].filter(src => src !== publication.image).map((src, index) => (
-                <Image
-                  key={index}
-                  src={src}
-                  alt={`Imagem ${index + 1} da publicação ${publication.title}`}
-                  width={300}
-                  height={200}
-                  className="w-full h-48 object-cover rounded-lg"
-                />
-              ))}
+              {imagesMap[id]
+                .filter((src) => src !== publication.image)
+                .map((src, index) => (
+                  <Image
+                    key={index}
+                    src={src}
+                    alt={`Imagem ${index + 1} da publicação ${
+                      publication.title
+                    }`}
+                    width={300}
+                    height={200}
+                    className="w-full h-48 object-cover rounded-lg"
+                  />
+                ))}
             </div>
           </div>
         )}
--- src/app/publicacoes/[id]/publications.ts
+++ src/app/publicacoes/[id]/publications.ts
@@ -7,7 +7,8 @@ export const publications = [
       "Obra viabilizada através do Edital Elizabete Anderle, foi publicada no ano de 2019, recebendo a premiação de 1° lugar na categoria arte popular. O livro foi desenvolvido a partir da pesquisa etnográfica realizada por membros do Instituto Tabuleiro, contendo história oral e registros fotográficos das comunidades pesqueiras do estuário do Rio da Madre. Essa é uma linha de atuação da entidade que busca a valorização da sociobiodiversidade e conservação dos atributos naturais dos territórios tradicionais que abrangem o Parque Estadual da Serra do Tabuleiro. A distribuição de mil exemplares do livro ocorreu de forma gratuita nas comunidades pesqueiras do Estuário do Rio da Madre e nas escolas. Duração: 2017/2019. Financiador: Prêmio Elizabete Anderle – 2017.",
     date: "2019-01-01",
     link: "/publicacoes/1",
-    image: "/3.1.%20Publica%C3%A7%C3%A3o%20do%20Livro%20%E2%80%9CCriador%20de%20Peixe%2C%20Criador%20de%20Gente_%20a%20pesca%2C%20a%20vida%20e%20a%20natureza%20do%20Estu%C3%A1rio%20do%20Rio%20da%20Madre%E2%80%9D/Canon%20EOS%206D_20180320_112232.Id_2750.jpg",
+    image:
+      "/3.1.%20Publica%C3%A7%C3%A3o%20do%20Livro%20%E2%80%9CCriador%20de%20Peixe%2C%20Criador%20de%20Gente_%20a%20pesca%2C%20a%20vida%20e%20a%20natureza%20do%20Estu%C3%A1rio%20do%20Rio%20da%20Madre%E2%80%9D/Canon%20EOS%206D_20180320_112232.Id_2750.jpg",
     type: "PUBLICAÇÃO DE LIVRO",
     resume:
       "Livro premiado sobre a pesca e a vida no Estuário do Rio da Madre, desenvolvido a partir de pesquisa etnográfica.",
@@ -19,7 +20,8 @@ export const publications = [
       "O programa de pesquisa Felinos do Tabuleiro tem como objetivo realizar estudos científicos acerca das espécies de felinos silvestres ocorrentes no Parque Estadual da Serra do Tabuleiro. Além disso, procura gerar subsídios técnicos para a conservação das espécies em escala local, nacional e internacional. As ações do programa são contínuas e frequentemente direcionadas para pesquisa no campo da ecologia e conservação destes mamíferos carnívoros. Duração: 2018 até o momento. Financiador: apoio Tigrinus Equipamentos para Pesquisa.",
     date: "2018-01-01",
     link: "/publicacoes/2",
-    image: "/3.2.%20Programa%20de%20Pesquisa%20%E2%80%9CFelinos%20do%20Tabuleiro%E2%80%9D/WhatsApp%20Image%202025-08-08%20at%2013.43.20%20(1).jpeg",
+    image:
+      "/3.2.%20Programa%20de%20Pesquisa%20%E2%80%9CFelinos%20do%20Tabuleiro%E2%80%9D/WhatsApp%20Image%202025-08-08%20at%2013.43.20%20(1).jpeg",
     type: "PROGRAMA DE PESQUISA",
     resume:
       "Programa de pesquisa sobre felinos silvestres no Parque Estadual da Serra do Tabuleiro, focado em ecologia e conservação.",
@@ -32,7 +34,8 @@ export const publications = [
       "O preá-de-Moleques-do-Sul (Cavia intermedia) é um pequeno e raro mamífero cujo habitat está restrito a apenas uma ilha de cerca de 10 hectares localizada no arquipélago de Moleques do Sul, zona intangível do Parque Estadual da Serra do Tabuleiro. A espécie figura entre os 20 pequenos mamíferos mais ameaçados do mundo e foi considerada Criticamente Ameaçada de Extinção em todos os níveis: global, nacional e estadual. Pensando nisso, o Instituto Tabuleiro, em parceria com o Instituto do Meio Ambiente do Estado de Santa Catarina (IMA), realizou a elaboração do Plano de Ação Estadual (PAE) para a conservação do Preá-de-Moleques-do-Sul. Duração: 2018/2019. Financiador: Fundação Boticário.",
     date: "2019-01-01",
     link: "/publicacoes/3",
-    image: "/3.3.%20%20Plano%20de%20A%C3%A7%C3%A3o%20Estadual%20(PAE)%20para%20a%20conserva%C3%A7%C3%A3o%20do%20Pre%C3%A1-de-Moleques-do-Sul/DSCF3874.JPG",
+    image:
+      "/3.3.%20%20Plano%20de%20A%C3%A7%C3%A3o%20Estadual%20(PAE)%20para%20a%20conserva%C3%A7%C3%A3o%20do%20Pre%C3%A1-de-Moleques-do-Sul/DSCF3874.JPG",
     type: "PLANO DE AÇÃO",
     resume:
       "Plano de ação para a conservação do preá-de-Moleques-do-Sul, espécie criticamente ameaçada de extinção.",
@@ -45,7 +48,8 @@ export const publications = [
       "O projeto tem por objetivo realizar diagnóstico da vegetação de restinga na Baixada do Massiambu através de amostragens fitossociológicas comparativas (entre as áreas onde houve incidência de queimadas recentes e as que não foram impactadas pelo fogo nos últimos 17 anos), como parâmetro para avaliar a integridade destes ambientes e inferir acerca dos efeitos de agentes de degradação dessa área do Parque Estadual da Serra do Tabuleiro. Deseja-se verificar se existem espécies-chave frente aos impactos antrópicos, as favorecidas e as não favorecidas no processo de regeneração natural. Duração: 2021 até o momento. Financiador: Transferência de valores de multa pecuniária por crime contra o meio ambiente em processo julgado pelo Ministério Público Federal / Procuradoria da República em Santa Catarina.",
     date: "2021-01-01",
     link: "/publicacoes/4",
-    image: "/3.4.%20Projeto%20de%20Pesquisa%20%E2%80%9CMonitoramento%20da%20flora%20da%20restinga%20da%20Baixada%20do%20Massiambu%20inserida%20no%20Parque%20Estadual%20da%20Serra%20do%20Tabuleiro_%20avalia%C3%A7%C3%A3o%20hist%C3%B3rica%20e%20correla%C3%A7%C3%A3o%20das%20condi%C3%A7%C3%B5es%20natural%20e%20p%C3%B3s-dist%C3%BArbios%20antr%C3%B3picos%E2%80%9D%20/Acisanthera%20aff%20variabilis%20cordaoCV%20Manoel%2014%20jan%20(2).JPG",
+    image:
+      "/3.4.%20Projeto%20de%20Pesquisa%20%E2%80%9CMonitoramento%20da%20flora%20da%20restinga%20da%20Baixada%20do%20Massiambu%20inserida%20no%20Parque%20Estadual%20da%20Serra%20do%20Tabuleiro_%20avalia%C3%A7%C3%A3o%20hist%C3%B3rica%20e%20correla%C3%A7%C3%A3o%20das%20condi%C3%A7%C3%B5es%20natural%20e%20p%C3%B3s-dist%C3%BArbios%20antr%C3%B3picos%E2%80%9D%20/Acisanthera%20aff%20variabilis%20cordaoCV%20Manoel%2014%20jan%20(2).JPG",
     type: "PROJETO DE PESQUISA",
     resume:
       "Projeto de monitoramento da flora da restinga na Baixada do Massiambu, avaliando impactos de queimadas e regeneração natural.",
@@ -58,7 +62,8 @@ export const publications = [
       "A única população conhecida de Cavia intermedia tem menos que 50 indivíduos adultos restrita em uma ilha de 10 ha, onde há outras endêmicas e um dos principais ninhais de aves marinhas do Atlântico Sul. O preá-de-moleques-do-sul, criticamente ameaçado de extinção em todos os níveis (global, nacional e estadual), está entre os 20 pequenos mamíferos mais ameaçados no mundo e entre as 100 espécies EDGE, mas a espécie ainda não possui um programa de monitoramento. O Plano de Ação de Conservação desta espécie foi elaborado em 2018-2019 pelo órgão responsável pela ilha (IMA) e o Instituto Tabuleiro. Entre as 28 ações deste Plano, uma busca contar e monitorar a espécie. Além disso, o monitoramento desta espécie demanda métodos quanto menos invasivos possíveis como, por exemplo, armadilha fotográfica. O objetivo deste novo projeto do IT é testar método não invasivo de monitoramento da população no seu ambiente através de armadilha fotográfica. Espera-se obter imagens suficientes para o banco de imagem da espécie e para estimativas populacionais por método não invasivo, assim como contribuir para pelo menos cinco ações do Plano de Ação do preá-de-moleques-do-sul. Duração: 2021/2022. Financiador: Global Wildlife Conservation/ Greensboro Science Center e IUCN.",
     date: "2021-01-01",
     link: "/publicacoes/5",
-    image: "/3.5.%20Projeto%20de%20Pesquisa%20%E2%80%9CAvalia%C3%A7%C3%A3o%20de%20procedimentos%20de%20monitoramento%20populacional%20n%C3%A3o%20invasivo%20do%20Pre%C3%A1-de-Moleques%20(Cavia%20intermedia)%20end%C3%AAmico%20da%20ilha%20de%20Moleques%20do%20Sul%2C%20Parque%20Estadual%20da%20Serra%20do%20Tabuleiro%E2%80%9D/2021-12-28%2011.13.44.jpg",
+    image:
+      "/3.5.%20Projeto%20de%20Pesquisa%20%E2%80%9CAvalia%C3%A7%C3%A3o%20de%20procedimentos%20de%20monitoramento%20populacional%20n%C3%A3o%20invasivo%20do%20Pre%C3%A1-de-Moleques%20(Cavia%20intermedia)%20end%C3%AAmico%20da%20ilha%20de%20Moleques%20do%20Sul%2C%20Parque%20Estadual%20da%20Serra%20do%20Tabuleiro%E2%80%9D/2021-12-28%2011.13.44.jpg",
     type: "PROJETO DE PESQUISA",
     resume:
       "Projeto de avaliação de métodos não invasivos para monitoramento populacional do preá-de-moleques-do-sul.",
@@ -71,7 +76,8 @@ export const publications = [
       "Commelina catharinensis é uma pequena erva rara, de ocorrência muito restrita ao litoral de Santa Catarina. Atualmente as únicas populações conhecidas estão localizadas na Baixada do Massiambú (Palhoça, SC). O projeto, sob coordenação do IT e IMA, foi firmado através de Acordo de Cooperação Técnica entre as partes, tendo início em abril de 2021. Visa a conservação de C. catharinensis e, tem como objetivo reduzir os riscos de extinção da espécie. Monitora indivíduos de ocorrência natural na Baixada do Massiambú e realiza introdução de nova população, visando ampliar a ocorrência desta para áreas menos vulneráveis dentro do PAEST. Das populações da Baixada, foram selecionados indivíduos matrizes para produção de mudas através de estacas. Em viveiro, foram produzidos 3 indivíduos, de onde, a partir de sementes destes, já foram produzidas e introduzidas mais de 100 mudas de C. catharinensis. As mudas foram plantadas em cordões arenosos situados nas proximidades do Centro de Visitantes do PAEST em três fases de plantio: em junho de 2022, março e novembro de 2023 e, em novembro de 2024. Como desdobramento do projeto foi criado um Programa de Manejo específico para conservação de C. catharinensis, onde, além das atividades já em andamento, prevê: - Realização de novos plantios de introdução; - Realização de expedições para encontrar a espécie em outras regiões do Estado e; - Buscar a avaliação do status de ameaça da espécie, fornecer subsídios e informar os órgãos responsáveis, visando a inclusão em listas de espécies da flora ameaçada. Duração: 2021 até o momento Financiador: sem financiador. Apoio: Viveiro Camarinha, C-Pack - Creative Packaging, Sociedade de Pesquisa em Vida Selvagem e Educação Ambiental (SPVS), Arteris Litoral Sul e Instituto Çarakura.",
     date: "2021-01-01",
     link: "/publicacoes/6",
-    image: "/3.6.%20Projeto%20de%20Pesquisa%20%E2%80%9CConserva%C3%A7%C3%A3o%20ex-situ%20de%20Commelina%20catharinensis%20Hassemer%20et%20al.%20(Commelinaceae)%E2%80%9D/C21%20abr%2023%20(47).JPG",
+    image:
+      "/3.6.%20Projeto%20de%20Pesquisa%20%E2%80%9CConserva%C3%A7%C3%A3o%20ex-situ%20de%20Commelina%20catharinensis%20Hassemer%20et%20al.%20(Commelinaceae)%E2%80%9D/C21%20abr%2023%20(47).JPG",
     type: "PROJETO DE PESQUISA",
     resume:
       "Projeto de conservação ex-situ de Commelina catharinensis, espécie rara do litoral de Santa Catarina.",
@@ -84,7 +90,8 @@ export const publications = [
       "O Preá-de-Moleques (Cavia intermedia) é endêmico da Ilha de Moleques-do-Sul, zona intangível do Parque Estadual da Serra do Tabuleiro (SC). Criticamente ameaçado em todos os níveis (global, nacional e estadual), é considerado um dos vinte pequenos mamíferos mais raros do planeta. As principais ameaças à conservação são o desembarque na ilha e ausência de informações, por parte do público em geral, sobre sua vulnerabilidade. O Programa de Educação Ambiental para a Conservação do Preá-de-Moleques ocorreu entre os anos de 2022/2024 em três comunidades costeiras do Estado, executado pelo Instituto do Tabuleiro e IMA-SC e financiado pela Karoon Energy. O Programa de Educação Ambiental para a Conservação do Preá-de-Moleques é composto por quatro projetos integrados: curso online para docentes; atividades educativas nas escolas; publicação de livro e educomunicação socioambiental, envolvendo diretamente três escolas das comunidades pesqueiras do sul de Florianópolis (Pântano do Sul) e do município de Palhoça (Pinheira e Guarda do Embaú), além dos pescadores e usuários de náuticas localizadas nestes municípios. O recorte espacial se deu pela proximidade com o Arquipélago de Moleques do Sul. O trabalho compõe a ação 4.1 do Plano de Ação Estadual para a Conservação do Preá-de Moleques-do-Sul (Cavia intermedia), elaborado pelo Instituto Tabuleiro em parceria com o Instituto do Meio Ambiente de Santa Catarina (IMA). O objetivo foi promover a divulgação científica e o engajamento social na conservação da espécie na Grande Florianópolis/SC. Além de atividades educativas nas escolas, foram elaborados e distribuídos materiais didáticos, realizados eventos socioambientais e reuniões com os pescadores para o intercâmbio de conhecimentos construídos com as comunidades escolares. Duração: 2022 até o momento Financiador: Karoon Petróleo e Gás Ltda.",
     date: "2022-01-01",
     link: "/publicacoes/7",
-    image: "/3.7.%20%20%E2%80%9CPrograma%20de%20Educa%C3%A7%C3%A3o%20Ambiental%20para%20Conserva%C3%A7%C3%A3o%20da%20esp%C3%A9cie%20end%C3%AAmica%20Cavia%20intermedia%2C%20o%20Pre%C3%A1%20do%20Arquip%C3%A9lago%20de%20Moleques%20do%20Sul%20-%20Santa%20Catarina%20-%20Brasil%E2%80%9D%20/IMG_2154-3.jpg",
+    image:
+      "/3.7.%20%20%E2%80%9CPrograma%20de%20Educa%C3%A7%C3%A3o%20Ambiental%20para%20Conserva%C3%A7%C3%A3o%20da%20esp%C3%A9cie%20end%C3%AAmica%20Cavia%20intermedia%2C%20o%20Pre%C3%A1%20do%20Arquip%C3%A9lago%20de%20Moleques%20do%20Sul%20-%20Santa%20Catarina%20-%20Brasil%E2%80%9D%20/IMG_2154-3.jpg",
     type: "PROGRAMA DE EDUCAÇÃO AMBIENTAL",
     resume:
       "Programa de educação ambiental para a conservação do Preá-de-Moleques em comunidades costeiras.",
@@ -97,7 +104,8 @@ export const publications = [
       "O projeto busca realizar a recuperação de uma área degradada em trecho de Mata Ciliar do Rio Maciambu, no interior da Terra Indígena (TI) Maciambu (Aldeia Pira Rupá), da etnia M’Bya Guarani, localizado ao sul do município de Palhoça, na região da Grande Florianópolis (SC). A área está inserida no bioma Mata Atlântica, limítrofe ao Parque Estadual da Serra do Tabuleiro (PAEST), a maior unidade de conservação de proteção integral do estado de Santa Catarina. As atividades do projeto estão ancoradas na troca de saberes entre indígenas moradores da TI e pesquisadoras do Instituto Tabuleiro, incluindo ações de reconhecimento e mapeamento do uso do território indígena, identificação etnobotânica de árvores nativas, monitoramento e marcação de matrizes para coleta de sementes, oficinas de viveiragem e mutirões de plantio de mudas nativas. A área a ser inicialmente recuperada é de aproximadamente 2.500 m2, onde serão plantadas 1000 mudas. Além do plantio de espécies da Mata Atlântica na mata ciliar, o projeto atua com espécies de valor cultural a partir da identificação etnobotânica de plantas que possuam usos medicinais, alimentícios e espirituais para a comunidade da Aldeia. Duração: 2025 até o momento Financiadores: Fundo Casa Socioambiental.",
     date: "2025-01-01",
     link: "/publicacoes/8",
-    image: "/3.8.%20Projeto_%20Refloresta%20Maciambu_%20Recupera%C3%A7%C3%A3o%20de%20mata%20ciliar%20e%20troca%20de%20saberes%20na%20Terra%20Ind%C3%ADgena%20Maciambu%20(Aldeia%20Pira%20Rup%C3%A1-SC)%20/Refloresta%20Maciambu%201.jpg",
+    image:
+      "/3.8.%20Projeto_%20Refloresta%20Maciambu_%20Recupera%C3%A7%C3%A3o%20de%20mata%20ciliar%20e%20troca%20de%20saberes%20na%20Terra%20Ind%C3%ADgena%20Maciambu%20(Aldeia%20Pira%20Rup%C3%A1-SC)%20/Refloresta%20Maciambu%201.jpg",
     type: "PROJETO",
     resume:
       "Projeto de recuperação de mata ciliar na Terra Indígena Maciambu, com troca de saberes entre indígenas e pesquisadores.",
